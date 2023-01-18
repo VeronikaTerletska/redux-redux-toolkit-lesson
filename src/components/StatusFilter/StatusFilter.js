23fac1b8b184fba6cd3a8890cfe3276e1d7f062b
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2023-01-18 19:06:51 +0200

    add redux

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,20 @@
+// Імпортуємо хук
+import { useSelector } from 'react-redux';
 import { Button } from 'components/Button/Button';
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from '../../redux/constans';
+import { getStatusFilter } from '../../redux/selectors';
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
